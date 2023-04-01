719b6d6d2ce268ce3bd0c377f50557fa176738c7
bencode <bencode@163.com>
2023-04-02 01:53:48 +0800

    feat: 完成第一个可用版本

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,9 +1,14 @@
+import * as pathUtil from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    outDir: pathUtil.join(__dirname, '../web/public')
+  },
 
   server: {
     host: true,
